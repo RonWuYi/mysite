82cb6215a9826a7d5fdac7762ee2d5ff35bafd08
Ron <lyshmily@outlook.com>
2017-09-17 11:03:44 +0200

    modified:   mysite/db.sqlite3 	modified:   mysite/mysite/settings.py 	modified:   mysite/polls/admin.py 	new file:   mysite/polls/migrations/0001_initial.py 	modified:   mysite/polls/models.py 	modified:   mysite/polls/views.py

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,18 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils import timezone
 
-# Create your models here.
+
+@python_2_unicode_compatible  # only if you need to support Python 2
+class Question(models.Model):
+    # ...
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+@python_2_unicode_compatible  # only if you need to support Python 2
+class Choice(models.Model):
+    # ...
+    def __str__(self):
+        return self.choice_text
