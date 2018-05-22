565fb50a2b0c4a54073d7afd0e0f2c3103ca80cc
Ron Wu <ron.wu@irdeto.com>
2018-05-22 16:18:34 +0200

    modified:   pyrate/models.py 	modified:   secondsite/settings.py

--- secondsite/secondsite/settings.py
+++ secondsite/secondsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pyrate.apps.PyrateConfig'
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
