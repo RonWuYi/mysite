9fe4404c345c1437f2f473c45ced02840bd1eb93
RonWuYi <lyshmily@outlook.com>
2019-06-06 17:10:03 +0200

    update polls

---
+++ mysite/polls/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+
+
+def index(request):
+    if request.method == 'GET':
+        return HttpResponse("this is GET request")
+    elif request.method == 'PUT':
+        return HttpResponse("this is PUT request")
+    elif request.method == 'POST':
+        return HttpResponse("this is POST request")
+    else:
+        return HttpResponse("we do not know which request it is")
