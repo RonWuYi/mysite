340b8df51d83d92a182be18a3eb979c4a7a4f7b6
RonWuYi <lyshmily@outlook.com>
2019-07-12 10:52:55 +0200

    add soap command KMS

--- test001.py
+++ test001.py
@@ -1,9 +1,33 @@
-import numpy as np
+import requests
 
-a = np.arange(15).reshape(3, 5)
+ip  = '172.16.98.51'
+url = 'http://{}/PIsys/Sector/Sector.asmx'.format(ip)
 
+operatorTag = '000'
+serialNumber = '2060713988'
+nationality = 'USA'
+regionTag = 'zz'
+productTagList1 = '1'
+productTagList2 = ''
+refreshSectorCreate = '1'
+locationTag = ''
+headers = {'Content-Type': 'application/soap+xml; charset=utf-8'}
 
-print(a)
+body = """<?xml version="1.0" encoding="utf-8"?>
+<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+  <soap12:Body>
+    <createSector xmlns="http://irdeto.com/pisys/sector">
+      <operatorTag>{}</operatorTag>
+      <serialNumber>{}</serialNumber>
+      <nationality>{}</nationality>
+      <regionTag>{}</regionTag>
+      <productTagList>
+        <string>{}</string>
+      </productTagList>
+    </createSector>
+  </soap12:Body>
+</soap12:Envelope>""".format(operatorTag, serialNumber, nationality, regionTag, productTagList1)
 
-print(a.shape);
-print(a.ndim);
+response = requests.post(url, data=body, headers=headers)
+print(response)
+print(response.reason)
