a3f75357eb72f4bed9f2e802fbbf70f93b992e1d
RonWuYi <lyshmily@outlook.com>
2019-07-16 14:57:48 +0200

    test

--- mysite/catalog/admin.py
+++ mysite/catalog/admin.py
@@ -2,7 +2,21 @@ from django.contrib import admin
 from catalog.models import Author, Genre, Book, BookInstance
 # Register your models here.
 
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(BookInstance)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+
+# admin.site.register(Author)
+
 admin.site.register(Genre)
+admin.site.register(Author, AuthorAdmin)
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
