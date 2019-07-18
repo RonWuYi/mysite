8cac3329c69a625c70d2200b9ef0a197691c6d9d
RonWuYi <lyshmily@outlook.com>
2019-07-18 09:19:16 +0200

    update requirement

--- tutor/snippets/models.py
+++ tutor/snippets/models.py
@@ -11,6 +11,9 @@ STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
 
 class Snippet(models.Model):
+    """
+    just a snippet
+    """
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
