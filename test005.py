061ec1781411046724b4ddeb7eab14a1f610c67e
RonWuYi <lyshmily@outlook.com>
2019-09-30 13:17:38 +0200

    chanage ignore file

--- test005.py
+++ test005.py
@@ -1,24 +1,3 @@
+from pathlib import Path
 
-# import os
-
-# if __name__ == '__main__':
-#     actions = {'create': create_user, 'login': login, 'change_password': change_password,
-#                 'help': help}
-
-#     try:
-#         action = sys.argv[1]
-#         print(action)
-#     except IndexError:
-#         action = 'help'
-#     args = sys.argv[2:]
-#     try:
-#         actions[action](*args)
-#         print(action)
-#     except (KeyError, TypeError):
-#         help()
-
-actions = {'create': create_user, 'login': login, 'change_password': change_password,
-                'help': help}
-
-
-print(actions.keys())
+print(Path.cwd())
