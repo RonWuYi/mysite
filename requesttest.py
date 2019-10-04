754475b37f76d8f2dcadac24d57e2b48edbdd192
RonWuYi <lyshmily@outlook.com>
2019-10-04 22:13:42 +0200

    delete all

--- requesttest.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-my_buck = 'bugs-246912.appspot.com'
-r = requests.get('https://www.googleapis.com/storage/v1/b/bugs-246912.appspot.com')
-print(r.status_code)
--- test018.py
+++
@@ -1,6 +0,0 @@
-import os
-
-folder_path = 'C:\\Work\\test project\\github\\mysite\\media'
-
-print(type(os.listdir(folder_path)))
-print(len(os.listdir(folder_path)))
